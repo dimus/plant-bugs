0326965a76bae104798d66b603e3171cbf63947d
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-01-21 16:28:43 -0600

    working data collection

--- col.rb
+++ col.rb
@@ -2,6 +2,7 @@
 
 require "mysql2"
 require "ostruct"
+require "json"
 
 class Db
   def self.query(query)
@@ -18,7 +19,7 @@ class Taxon < OpenStruct
     return self.references[id] if self.references[id]
     res = Db.query("select
                        d.name as distribution, rc.description as comment,
-                       b.title, b.year, b.pub_year, p.name,
+                       b.title, b.year, b.pub_year, p.name as details,
                        a.long_name, a.short_name
                      from reference r
                        join biblio_instance bi on bi.id = r.biblio_instance_id
@@ -28,7 +29,15 @@ class Taxon < OpenStruct
                        join publication p on p.id = b.publication_id
                        join authority a on a.Weeks=bi.primary_authority_id
                      where r.id = #{id}").first
-   self.references[id] = OpenStruct.new(res.merge(original: original)) if res
+   self.references[id] = Reference.new(res.merge(original: original)) if res
+  end
+
+  def populate_refs
+    reference(orig_ref_id, true)
+    res = Db.query("select id from reference where taxon_id = #{id}")
+    res.each do |r|
+      reference(r["id"], r["id"] == orig_ref_id) unless references[r["id"]]
+    end
   end
 
   def authorship
@@ -51,7 +60,13 @@ class Taxon < OpenStruct
 end
 
 class Reference < OpenStruct
-
+  # def reformat_authors
+  #   first_author = /^(([^,]+),(\s*[A-Z]\.*?)+(,\s*Jr\.)?)(,|and|$)/
+  #   puts long_name
+  #   match = long_name.match(first_author)
+  #   au = match ? match[1] : "???"
+  #   puts au
+  # end
 end
 
 class Col
@@ -64,21 +79,49 @@ class Col
 
   def valid_taxa
     Db.query("select * from taxon where availability='valid'")
+             # and id in (11428, 146, 14617, 10154, 16342)")
   end
 
   def traverse_taxa
+    res = []
     valid_taxa.each do |t|
       @taxon = Taxon.new(t.merge(references: {}))
-      @taxon.reference(@taxon.orig_ref_id, true)
-      puts @taxon.sci_name
+      @taxon.populate_refs
+      taxon = { id: @taxon.id, name: @taxon.sci_name, refs: [], syns: [] }
       @taxon.references.each do |k, v|
-        puts "  %s %s" % [k, v.comment]
+        taxon[:refs] << { id: k, orig: v.original,  author: v.long_name,
+                         year: v.pub_year, title: v.title, details: v.details,
+                         distribution: v.distribution, comment: v.comment}
       end
+      taxon[:syns] = collect_synonyms
+      res << taxon
     end
+    puts res.to_json
   end
 
+  def collect_synonyms
+    res = []
+    synonyms = Db.query("select *
+                           from taxon
+                           where id in (
+                             select junior_taxon_id
+                               from synonym
+                               where senior_taxon_id = #{@taxon.id}
+                                 and junior_taxon_id != senior_taxon_id)")
+    synonyms.each do |s|
+      @synonym = Taxon.new(s.merge(references: {}))
+      @synonym.populate_refs
+      taxon = { id: @synonym.id, name: @synonym.sci_name, refs: [] }
+      @synonym.references.each do |k, v|
+        taxon[:refs] << { id: k, orig: v.original,  author: v.long_name,
+                         year: v.pub_year, title: v.title, details: v.details,
+                         distribution: v.distribution, comment: v.comment}
+      end
+      res << taxon
+    end
+    res
+  end
 end
-
 
 col = Col.new
 col.traverse_taxa
